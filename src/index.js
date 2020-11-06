aa8b33a301354ea15ec4edff0ef351ba86bc202a
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-06 17:24:30 -0600

    creating store. wrapping <App> with <Provider>. stubbing in feedbackReducer and its initial state

--- src/index.js
+++ src/index.js
@@ -4,5 +4,38 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux Dependencies
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// Redux Reducers
+// --------------------------
+
+const feedbackReducerInitialState = {
+  feeling: '',
+  understanding: '',
+  support: '',
+  comments: '',
+};
+
+const feedbackReducer = (state = feedbackReducerInitialState, action) => {
+  // set up different action.type conditional checks that utilize spread to update individual key/value pairs on initial state
+  return state;
+};
+
+const storeInstance = createStore(
+  combineReducers({
+    feedbackReducer,
+  }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
