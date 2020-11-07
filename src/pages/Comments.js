24084bbb71c8dbe9e38d7854169a28c4f9bfd2a1
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-07 11:30:01 -0600

    setting up onChange listener for comments input field

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class Comments extends Component {
+  state = {
+    comments: '',
+  };
+
+  onCommentChange = (event) => {
+    this.setState(
+      {
+        comments: event.target.value,
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
   render() {
     return (
       <div>
         <p>Any comments you want to leave?</p>
+        <input
+          onChange={this.onCommentChange}
+          placeholder="Leave your comments here"
+        ></input>
         <button>DONE!</button>
       </div>
     );
