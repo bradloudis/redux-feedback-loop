03203bbb91228a8f1666046cb93bce6317fc083d
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-07 13:23:16 -0600

    using mapStoreToProps to update page with info from feedbackReducer

--- src/pages/ReviewFeedback.js
+++ src/pages/ReviewFeedback.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class ReviewFeedback extends Component {
   render() {
     return (
       <div>
         <p>Review your feedback and submit!</p>
+        <p>
+          feeling: <span>{this.props.store.feedbackReducer.feeling}</span>
+        </p>
+        <p>
+          understanding:
+          <span>{this.props.store.feedbackReducer.understanding}</span>
+        </p>
+        <p>
+          support: <span>{this.props.store.feedbackReducer.support}</span>
+        </p>
+        <p>
+          comments: <span>{this.props.store.feedbackReducer.comments}</span>
+        </p>
       </div>
     );
   }
 }
 
-export default ReviewFeedback;
+const mapStoreToProps = (store) => ({
+  store,
+});
+
+export default connect(mapStoreToProps)(ReviewFeedback);
