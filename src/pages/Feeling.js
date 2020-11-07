17de372329781f8e6e0537dd7a9dcc00b2a40373
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-07 12:37:43 -0600

    adding radio buttons to Feeling.js. handleRadioChange allows dynamic rendering to show which btn has been selected

--- src/pages/Feeling.js
+++ src/pages/Feeling.js
@@ -1,16 +1,80 @@
 import React, { Component } from 'react';
 
 class Feeling extends Component {
+  state = {
+    feeling: '',
+  };
+
   onButtonClick = (event) => {
+    event.preventDefault();
     this.props.history.push('/understanding');
+  };
+
+  handleRadioChange = (event) => {
+    this.setState({
+      feeling: event.target.value,
+    });
   };
 
   render() {
     return (
-      <div>
-        <p>How are you feeling today?</p>
+      <form>
+        <div>
+          <p>How are you feeling today?</p>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="feeling"
+            value="1"
+            checked={this.state.feeling === '1'}
+            onChange={this.handleRadioChange}
+            required
+          />
+          <label>1</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="feeling"
+            value="2"
+            checked={this.state.feeling === '2'}
+            onChange={this.handleRadioChange}
+          />
+          <label>2</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="feeling"
+            value="3"
+            checked={this.state.feeling === '3'}
+            onChange={this.handleRadioChange}
+          />
+          <label>3</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="feeling"
+            value="4"
+            checked={this.state.feeling === '4'}
+            onChange={this.handleRadioChange}
+          />
+          <label>4</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="feeling"
+            value="5"
+            checked={this.state.feeling === '5'}
+            onChange={this.handleRadioChange}
+          />
+          <label>5</label>
+        </div>
         <button onClick={this.onButtonClick}>Next</button>
-      </div>
+      </form>
     );
   }
 }
