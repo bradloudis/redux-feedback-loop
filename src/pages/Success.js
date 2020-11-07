93f9b20631a27f92f284d03819aa5c4acee8d7cd
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-07 17:13:05 -0600

    adding <Link> to start the feedback loop over at Feeling.js

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Success extends Component {
   render() {
     return (
       <div>
         <p>Your feedback has been successfully submitted!</p>
+        <p>
+          <Link to="/feeling">Click here</Link> to start over.
+        </p>
       </div>
     );
   }
